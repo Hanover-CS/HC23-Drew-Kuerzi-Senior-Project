c55e8f63d42d4d97ddf392a5d07324fab03dc34d
Dkuerzi <70341685+Dkuerzi@users.noreply.github.com>
2022-12-05 21:54:53 -0500

    Added javadoc to HomeScreen.java and Schedule.java Ref #13

--- app/src/main/java/com/example/gamelog/HomeScreen.java
+++ app/src/main/java/com/example/gamelog/HomeScreen.java
@@ -6,16 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ * Activity for loading NFL teams on the front page
+ */
 public class HomeScreen extends AppCompatActivity {
-
     private static String team;
 
+    /**
+     * @param savedInstanceState
+     * Method that is automaticality ran when the activity is created
+     * Sets the view to the home_screen layout to display buttons
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
     }
 
+    /**
+     * @param view
+     * Sets the team name to the button pressed
+     * Starts the next activity
+     */
     public void nextPage(View view){
         team = view.getTag().toString();
         Intent i = new Intent(HomeScreen.this, Schedule.class);
@@ -23,6 +35,9 @@ public class HomeScreen extends AppCompatActivity {
 
     }
 
+    /**
+     * @return The team name is returned to be accessible between activities.
+     */
     public static String getTeam() {
         return team;
     }
--- app/src/main/java/com/example/gamelog/Schedule.java
+++ app/src/main/java/com/example/gamelog/Schedule.java
@@ -8,9 +8,17 @@ import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
+/**
+ * Activity for displaying the team selected and displaying all available weeks to choose
+ */
 public class Schedule extends AppCompatActivity {
     private static String week;
 
+    /**
+     * @param savedInstanceState
+     * Method that is automaticality ran when the activity is created
+     * Sets the view to the schedule layout to display buttons
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +28,20 @@ public class Schedule extends AppCompatActivity {
         team.setText(HomeScreen.getTeam());
     }
 
+    /**
+     * @param view
+     * Sets the week to the button pressed
+     * Starts the next activity
+     */
     public void nextPage(View view){
         week = view.getTag().toString();
         Intent i = new Intent(Schedule.this, BoxScore.class);
         startActivity(i);
     }
 
+    /**
+     * @return The week is returned to be accessible between activities.
+     */
     public static String getWeek(){
         return week;
     }
